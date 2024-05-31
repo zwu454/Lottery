312cdff50a8fc7244aa1a96041be118986f7b60b
zwu454 <162061272+zwu454@users.noreply.github.com>
2024-05-31 14:17:04 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
         List<Employee> employeeList = new ArrayList<>();
